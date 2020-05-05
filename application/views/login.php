1dd11b98d778547f9fe34a93be803f6c77f72a23
Muhammad Zidane Nahla Qowy <muhammadzidanenahlaqowy@gmail.com>
2020-05-05 23:35:24 +0700

    first upload

---
+++ application/views/login.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Login Page</title>
+
+	<style type="text/css">
+//css
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Login!</h1>
+
+<a href="/PROJECTRNDXFAVE/register">register here</a>
+</div>
+
+</body>
+</html>
---
+++ application/views/register.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Register</title>
+
+	<style type="text/css">
+//css
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Register!</h1>
+
+	<a href="/PROJECTRNDXFAVE/login">login here</a>
+</div>
+
+</body>
+</html>
